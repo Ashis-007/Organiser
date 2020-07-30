07fc23ebbf86549e13ce097fd4b20a21a0e86b34
Ashis-007 <ashisjc@gmail.com>
2020-07-30 21:58:35 +0530

    Minor changes

--- cleaner.py
+++ cleaner.py
@@ -3,21 +3,20 @@ import os
 import shutil
 
 file_types = {
-    "pdf": ["pdf"],
+    "PDF": ["pdf"],
     "Image": ["jpg", "jpeg", "png", "gif", "svg", "ico", "tif", "tiff", "bmp", "psd"],
     "Video": ["mp4", "mkv", "3gp", "avi", "m4v", "mpg", "mpeg", "wmv"],
     "Audio": ["mp3", "wav", "wma",],
     "Document": ["doc","docx", "odt", "rtf"],
     "Spreadsheet": ["xls", "xlxs"],
-    "ppt": ["ppt", "pptx"],
+    "PPT": ["ppt", "pptx"],
     "Text": ["txt"],
     "zip": ["zip", "7z", "rar"],
-    "fonts": ["ttf", "otf", "fnt", "fon"],
-    "programming" : ["js", "cpp", "java", "py", "c", "cs", "css", "html", "php"]
+    "Fonts": ["ttf", "otf", "fnt", "fon"],
+    "Programming" : ["js", "cpp", "java", "py", "c", "cs", "css", "html", "php"]
 }
 
 path_name = repr(sys.argv[1]) 
-print(path_name)
 
 for file in os.listdir(path_name):
     file_name = file.lower()
